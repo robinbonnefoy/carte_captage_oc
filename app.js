b228e1326e42c357204d6a9888d32de0219a0efc
robinbonnefoy <robinbonnefoy@orange.fr>
2024-07-29 16:55:38 +0200

    popupCaptage - lien demarche

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     ///// Constantes
 
     const panelWidth = "300px";
+    const fieldset_captages = document.getElementById('onglet_captages');
+    const fieldset_demarches = document.getElementById('onglet_demarches');
     
     // ------------------------------------------------------------------------------------------------------------
     ///// Méthodes
@@ -202,16 +204,13 @@ $(document).ready(function () {
         //aac.bringToFront();
     };
 
-    // Afficher les infos de la démarche lors du clic
-    function showDemarcheInfo(feature) {
-        openPanel(); // Ouvre le panneau latéral si nécessaire
-        afficherOnglet('onglet_demarches'); // ouvrir l'oonglet Dméraches du panneau
-        const infoContent = document.getElementById('onglet_demarches');
-        infoContent.innerHTML = `
+    // Remplir les infos de la démarche de la Partie "Démarche territoriale"
+    function fillDemarcheInfo(feature){
+        fieldset_demarches.innerHTML = `
             <p class="popup_demarche_titre"> ${feature.properties.nom} </p>
             <span class="popup_demarche_partie"> Type de démarche </span>
             <span class="popup_info"> ${feature.properties.type} </span>
-            <span class="popup_demarche_partie"> Structure en charge de la structure </span>
+            <span class="popup_demarche_partie"> Structure en charge de l'animation </span>
             <span class="popup_info"> ${feature.properties.structure_animation} </span>
             <span class="popup_demarche_partie"> Animatrice(teur) </span>
             <span class="popup_info"> ${feature.properties.animateur} </span>
@@ -249,6 +248,24 @@ $(document).ready(function () {
         // Paramétrage du popup (sections rabatues)
         moreInfoAction();
     }
+
+    // Remplir la démarche à partir de l'id_demarche_web
+    function fillDemarcheFromIDWeb (idDemarcheWeb){
+        demarches.eachLayer(function (layer) {
+            // Si le captage est associé à la démarche
+            if (layer.feature.properties.id_demarche_web === idDemarcheWeb) {
+                // On complète les infos du captage
+                fillDemarcheInfo(layer.feature);
+            }
+        });
+    }
+
+    // Afficher les infos de la démarche lors du clic
+    function showDemarcheInfo(feature) {
+        openPanel(); // Ouvre le panneau latéral si nécessaire
+        afficherOnglet('onglet_demarches'); // ouvrir l'onglet Dméraches du panneau
+        fillDemarcheInfo(feature);
+    }
     
 
     // Appliqué à chaque démarche
@@ -264,6 +281,8 @@ $(document).ready(function () {
             click: function (e) {
                 zoomToFeature(e);
                 showDemarcheInfo(feature);
+                reinitializeCaptage(); // Réinitialiser les infos de la partie "Captages associé(s)"
+                fillAllCaptagesInfo(feature.properties.id_demarche_web); // Complète les infos de la partie "Captages associé(s)"
             }
         });
     };
@@ -371,15 +390,111 @@ $(document).ready(function () {
         marker.setIcon(icon);
     }
 
+    // Fonction de zoom lors du click sur un captage
+    function clickZoom(e) {
+        //map.setView(e.target.getLatLng(), 13);
+        map.flyTo(e.target.getLatLng(), 10, {
+            duration: 0.5,  // Durée du vol en secondes
+            maxZoom: 10     // Niveau de zoom maximal
+        });
+    }
+
+    // Réinitialiser le contenu du fieldset Captages
+    function reinitializeCaptage () {
+        fieldset_captages.innerHTML = ''; // Réinitialiser le contenu du fieldset
+    }
+
+    // Construit les infos d'un captage
+    function CaptageInfo(feature) {
+        var properties = feature.properties;
+        return (`
+            <div class"captage_title"> 
+                ${properties.nom_ouvrage}
+                <span class="toggle-icon"></span>
+            </div>
+            <div class="captage-details">
+                <p><strong>Type :</strong> ${properties.type}</p>
+                <p><strong>Structure en charge :</strong> ${properties.structure_animation}</p>
+                <p><strong>Animateur :</strong> ${properties.animateur}</p>
+                <p><strong>Email :</strong> ${properties.mail}</p>
+                <p><strong>Téléphone :</strong> ${properties.telephone}</p>
+            </div>
+        `);
+    }
+    
+    // Contruit le conteneur des infos d'un captage
+    function fillCaptageInfo (feature){
+        var captageInfo = document.createElement('div');
+        captageInfo.className = 'captage-info';
+        captageInfo.innerHTML = CaptageInfo(feature);
+        captageInfo.id = feature.properties.id_captage_web; // id du conteneur captage-info est id_captage_web
+        fieldset_captages.appendChild(captageInfo);
+        captageInfo.addEventListener('click', function(event) {
+            event.stopPropagation();
+            toggleCaptageDetails(captageInfo);
+        });
+    }
+
+
+    // Complète les infos de tous les captages d'une démarche
+    function fillAllCaptagesInfo(idDemarcheWeb) {
+        // Pour l'ensemble des captages
+        captages.eachLayer(function (layer) {
+            // Si le captage est associé à la démarche
+            if (layer.feature.properties.id_demarche_web === idDemarcheWeb) {
+                // On complète les infos du captage
+                fillCaptageInfo(layer.feature);
+            }
+        });
+    }
+
+    // Changer le style du captage sélectionné
+    function toggleCaptageDetails(element) {
+        var allCaptages = document.querySelectorAll('.captage-info');
+        allCaptages.forEach(function(captage) {
+            if (captage !== element) {
+                captage.classList.remove('expanded');
+            }
+        });
+        element.classList.toggle('expanded');
+    }
+
+    // Déplie le popup du captage sélectionné
+    function openPopupSelectedCaptage(idCaptageWeb){
+        var conteneurSelectedCaptage = document.getElementById(idCaptageWeb);
+        toggleCaptageDetails(conteneurSelectedCaptage);
+    }
+
+    // Lors du clic sur un captage
+    function showPopupCaptage(feature){
+        var idDemarcheWeb = feature.properties.id_demarche_web;
+        var idCaptageWeb = feature.properties.id_captage_web;
+        openPanel(); // Ouvre le panneau latéral si nécessaire
+        afficherOnglet('onglet_captages'); // ouvrir l'onglet Dméraches du panneau
+        reinitializeCaptage(); // Réinitaliser le contenu du fieldset captage
+        if (idDemarcheWeb === 'non') {
+            // pas de démarche associée, on affiche les infos du captage
+            fillCaptageInfo(feature);
+        } else {
+            // une démarche associée, on affiche les infos de tous les capatges de la démarches
+            fillAllCaptagesInfo(idDemarcheWeb); // Compléter le contenu du fieldset captage
+        }
+        openPopupSelectedCaptage(idCaptageWeb); // déplie le popup du captage sélectionné
+    }
+
+    
     function onEachFeature_captage(feature, layer){
         // layer.bindPopup(popup_content(feature,'Établissement de tourisme et de loisir','camping',false)
         // );
         layer.on({
-            // click : clickZoom,
             mouseover : highlightMarker,
             mouseout : resetHighlightMarker,
+            click: function (e) {
+                clickZoom(e);
+                showPopupCaptage(feature);
+                fillDemarcheFromIDWeb(feature.properties.id_demarche_web);
+            }
           });
-
     }
 
     const captages = L.geoJSON(null, {
@@ -418,6 +533,17 @@ $(document).ready(function () {
     // Ajouter le contrôle à la carte (mais il sera visible seulement quand nécessaire)
     var layerHiddenControl = new LayerHiddenControl({ position: 'bottomright' });
 
+    // Récupérer les captages liés à la démarche
+    function getLinkedCaptages (IdDemarcheWeb) {
+        captages.eachLayer (function (layer){
+            if (layer.feature.properties.id_demarche_web === IdDemarcheWeb) {
+                // layer.setStyle(captageHighlightStyle());
+                console.log(layer.feature.properties.id_captage_web);
+            } else {
+                // layer.setStyle(resetHighlightMarker());
+            }
+        });
+    }
 
     $.getJSON('data/captages.geojson', function(data){
         captages.addData(data).addTo(map);
@@ -427,7 +553,8 @@ $(document).ready(function () {
     // Mise à jour de la visibilité de la carte lors du zoom / dézoom
     map.on('zoomend', updateGeoJSONLayerVisibility);
 
+    // --------------------------------------------------------------------------------------------------------------
 
-    map.whenReady(chargement_carte); // une fois la carte chargee, on efface l'icone de chargement
+    map.whenReady(chargement_carte); // Une fois la carte chargee, on efface l'icone de chargement
 
 }); // fin JQuery
